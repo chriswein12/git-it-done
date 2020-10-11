82b907d5df167bdee31b4487635b6df77b45f4fe
Chris Wein <chriswein12@yahoo.com>
2020-10-10 19:10:41 -0500

    Added error checking to issues list

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,5 +1,18 @@
 var issuesContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
+var repoNameEl = document.querySelector("#repo-name");
+
+var getRepoName = function () {
+    var queryString = document.location.search;
+    var repoName = queryString.split("=")[1];
+    if(repoName) {
+        getRepoIssues(repoName);
+        repoNameEl.textContent = repoName
+    }
+    else {
+        document.location.replace("./index.html");
+    }
+}
 
 var getRepoIssues = function(repo) {
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
@@ -15,7 +28,7 @@ var getRepoIssues = function(repo) {
             });
         }
         else {
-            alert("There was a problem with your request!");
+            document.location.replace("./index.html");
         }
     });
     
@@ -70,4 +83,4 @@ var displayWarning = function(repo) {
     limitWarningEl.appendChild(linkEl);
   };
 
-getRepoIssues("facebook/react");
+getRepoName();
